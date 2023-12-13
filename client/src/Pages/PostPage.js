d497d5b2af9c3c537a42a13fd0a8fd5a00341cb5
Isaque <isaquefrankli@gmail.com>
2023-12-13 20:44:58 -0300

    reading post page almost done

--- client/src/Pages/PostPage.js
+++ client/src/Pages/PostPage.js
@@ -14,7 +14,15 @@ export default function PostPage() {
         })
     }, []);
 
+    if (!postInfo) return '';
+
     return (
-        <div>Post page aqui.</div>
+        <div className='post-page'>
+            <div className='image'>
+                <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+            </div>
+            <h1>{postInfo.title}</h1>
+            <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
+        </div>
     )
 }
