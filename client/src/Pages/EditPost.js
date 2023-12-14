182490a65a6ce1a75235fbd158cc7099708e72c1
Isaque <isaquefrankli@gmail.com>
2023-12-14 09:22:35 -0300

    edit post page and api working

--- client/src/Pages/EditPost.js
+++ client/src/Pages/EditPost.js
@@ -13,32 +13,34 @@ export default function EditPost() {
     const [redirect, setRedirect] = useState(false);
 
     useEffect(() => {
-        fetch('http://localhost:4000/post/'+id).then(response => {
+        fetch('http://localhost:4000/post/'+id)
+          .then(response => {
             response.json().then(postInfo => {
-                setTitle(postInfo.title);
-                setContent(postInfo.content);
-                setSummary(postInfo.summary);
-
-            })
-        })
-    }, [])
+              setTitle(postInfo.title);
+              setContent(postInfo.content);
+              setSummary(postInfo.summary);
+            });
+          });
+    }, []);
 
     async function updatePost(ev) {
         ev.preventDefault();
-
-        const data = new FormatData();
+        const data = new FormData();
         data.set('title', title);
         data.set('summary', summary);
         data.set('content', content);
-        if(files?.[0]) {
-            data.set('file', files?.[0]);
+        data.set('id', id);
+        if (files?.[0]) {
+          data.set('file', files?.[0]);
         }
-
-        await fetch('http://localhost:4000/post', {
-            method: 'PUT',
-            body: data,
+        const response = await fetch('http://localhost:4000/post', {
+          method: 'PUT',
+          body: data,
+          credentials: 'include',
         });
-        setRedirect(true);
+        if (response.ok) {
+          setRedirect(true);
+        }
     }
 
 
