bdf11bc0a81a0c31a26e9aed74c1e12470260085
Isaque <isaquefrankli@gmail.com>
2023-12-13 21:17:35 -0300

    reading post page done

--- client/src/Pages/PostPage.js
+++ client/src/Pages/PostPage.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
+import {formatISO9075, format} from "date-fns";
+
 
 export default function PostPage() {
 
@@ -21,6 +23,8 @@ export default function PostPage() {
             <div className='image'>
                 <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
             </div>
+            <time>{format(new Date(postInfo.createdAt), "d MMM, yyyy HH:mm")}</time>
+            <div className='author'>Escrito por @{postInfo.author.username}</div>
             <h1>{postInfo.title}</h1>
             <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
         </div>
